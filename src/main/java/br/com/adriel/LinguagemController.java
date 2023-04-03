0712818db362a9fc3f538e3416b8dee244e24d9c
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-03 19:39:40 -0400

    Listar por order de ranking

--- src/main/java/br/com/adriel/LinguagemController.java
+++ src/main/java/br/com/adriel/LinguagemController.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,7 +21,7 @@ public class LinguagemController {
 	
 	@GetMapping("/linguagens")
 	public List<Linguagem> obterLinguagens(){
-		List<Linguagem> linguagens = repositorio.findAll();
+		List<Linguagem> linguagens = repositorio.findByOrderByRanking();
 		return linguagens;
 	}
 	

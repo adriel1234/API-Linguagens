d905a962c86972ef5dfe56a147880451a2e2459f
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-03 18:36:58 -0400

    cadastrar e consultar realizado

---
+++ src/main/java/br/com/adriel/Linguagem.java
@@ -0,0 +1,51 @@
+package br.com.adriel;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
+public class Linguagem {
+	
+	@Id
+	private String id;
+	private String titulo;
+	private String imagem;
+	private int ranking;
+	
+	public Linguagem() {
+		
+	}
+	
+	public Linguagem(String titulo, String imagem, int ranking) {
+		super();
+		this.titulo = titulo;
+		this.imagem = imagem;
+		this.ranking = ranking;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	public int getRanking() {
+		return ranking;
+	}
+	public void setRanking(int ranking) {
+		this.ranking = ranking;
+	}
+	
+	
+
+}
---
+++ src/test/java/br/com/adriel/ApiLinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLinguagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
